ce2902f25dc00e8e09a4e4825a3e6eb16ffc2208
GQ <GQ@GitBashHere.com>
2023-03-05 15:16:43 +0800

    gitee Test

--- src/main/java/git/test/JavaTest01.java
+++ src/main/java/git/test/JavaTest01.java
@@ -8,7 +8,7 @@ public class JavaTest01 {
         System.out.println("master01");
         System.out.println("hot-fix2");
         System.out.println("push git-test");
-    System.out.println("pull git-test");
+        System.out.println("pull git-test");
         System.out.println("pull gitee-test");
          System.out.println("pull gitee-test1");
     }
